afce44130c52acc2fe8760344502bffb47da8dac
Ahmed M. Ezzeldeen <Ahmed.Mohamed.Ezzeldeen@Gmail.com>
2025-02-17 22:03:19 +0200

    :art: Update HomePage layout: adjust padding for improved spacing and consistency

--- pages/Home/HomePage.tsx
+++ pages/Home/HomePage.tsx
@@ -7,7 +7,7 @@ export default function HomePage() {
   return (
     <>
       <Header />
-      <main className="max-w-screen-xl mx-auto px-16 md:pl-10 pt-20 flex-col flex">
+      <main className="max-w-screen-xl mx-auto p-6 md:pl-10 pt-20 flex-col flex">
         <HeroSection />
       </main>
       <Footer/>
