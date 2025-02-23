81390d48c255285930871bdcbdf206baddf4d7e3
Ahmed M. Ezzeldeen <Ahmed.Mohamed.Ezzeldeen@Gmail.com>
2025-02-23 04:44:35 +0200

    :heavy_plus_sign: Add project and partner model type definitions: define interfaces for project and partner data structures

--- types/database.d.ts
+++ types/database.d.ts
@@ -24,3 +24,31 @@ export interface DEventModel extends DEventTimeSlot {
   agenda: DEventTimeSlot[]; // List of time slots (sessions in the agenda)
   relatedProjects: string[]; // List of related project IDs
 }
+
+// Project Model Type Definition
+
+export interface DProjectMember {
+  name: string;
+  role: string;
+  imageSrc: string;
+  url?: string | null;
+}
+
+export interface DProjectModel {
+  id: string; // Unique project ID
+  title: string;
+  description: string;
+  videoUrl: string | null; // URL for project video
+  url: string | null; // URL for project link
+  teamMembers: DProjectMember[];
+  relatedEvents: string[] | null; // List of related event IDs
+  relatedEventsTitles: string ; // Title for related events section
+  technologies: string[] | null; // List of technologies used in the project
+}
+
+// Partner Model Type Definition
+export interface DPartnerModel {
+  name: string;
+  url: string;
+  imageURL: string;
+}
