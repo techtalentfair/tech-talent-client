d465faad2923d44d2f679de6fca2e39df7fd1ebf
Ahmed M. Ezzeldeen <Ahmed.Mohamed.Ezzeldeen@Gmail.com>
2025-02-24 16:32:06 +0200

    :art: Refactor layout structure: add Header and Footer components to RootLayout and simplify HomePage

--- pages/Home/HomePage.tsx
+++ pages/Home/HomePage.tsx
@@ -1,5 +1,3 @@
-import Header from "@/components/Header"
-import Footer from "@/components/Footer";
 import HeroSection from "@/pages/Home/Sections/HeroSection"
 import UpcomingEventsSection from "@/pages/Home/Sections/UpcomingEventsSection";
 import OurPartnersSection from "@/pages/Home/Sections/OurPartnersSection";
@@ -9,14 +7,10 @@ import ProjectsSection from "@/pages/Home/Sections/ProjectsSection";
 export default function HomePage() {
   return (
     <>
-      <Header />
-      <main className="max-w-screen-xl mx-auto p-6 md:pl-10 pt-20 flex-col flex">
-        <HeroSection />
-        <UpcomingEventsSection />
-        <OurPartnersSection />
-        <ProjectsSection />
-      </main>
-      <Footer/>
+      <HeroSection />
+      <UpcomingEventsSection />
+      <OurPartnersSection />
+      <ProjectsSection />
     </>
   );
 }
