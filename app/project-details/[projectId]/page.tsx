d2d71ed54524a9375a346d78b85689d6c2bfe213
Ahmed M. Ezzeldeen <Ahmed.Mohamed.Ezzeldeen@Gmail.com>
2025-02-24 16:49:21 +0200

    :bug: fix(project-details): resolve type error in dynamic route params

--- app/project-details/[projectId]/page.tsx
+++ app/project-details/[projectId]/page.tsx
@@ -1,11 +1,12 @@
 import ProjectDetailsPage from "@/pages/ProjectDetailsPage/ProjectDetailsPage";
 import React from "react";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default function ProjectDetails(props: { params: { projectId: any } }) {
-  const projectId = props.params.projectId;
+interface ProjectDetailsProps {
+  params: {
+    projectId: string;
+  };
+}
 
-  return (
-    <ProjectDetailsPage projectId={projectId} />
-  );
+export default function ProjectDetails({ params }: ProjectDetailsProps) {
+  return <ProjectDetailsPage projectId={params.projectId} />;
 }
