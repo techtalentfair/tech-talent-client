1866b4aa22e0634237d157f76cc203437c86cef6
Ahmed M. Ezzeldeen <Ahmed.Mohamed.Ezzeldeen@Gmail.com>
2025-02-28 16:11:04 +0200

    :art: format EventsPage component for improved readability

--- pages/Events/EventsPage.tsx
+++ pages/Events/EventsPage.tsx
@@ -3,15 +3,15 @@ import ImageSectoin from "./Sections/ImageSection";
 import CardEvents from "./Sections/CardEvents";
 import EventsList from "@/components/EventsList";
 import RegisterSectionComponent from '@/components/RegisterSectionComponent';
-import { agendaData , exampleEvent} from '@/data/eventsData'
+import { agendaData, exampleEvent } from '@/data/eventsData'
 
 export default function EventsPage() {
   return (
     <>
-        <ImageSectoin />
-        <CardEvents />
-        <RegisterSectionComponent eventId={exampleEvent.id} />
-        <EventsList  eventsData={agendaData}/>
+      <ImageSectoin />
+      <CardEvents />
+      <RegisterSectionComponent eventId={exampleEvent.id} />
+      <EventsList eventsData={agendaData} />
     </>
   );
 }
